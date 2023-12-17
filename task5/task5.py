30845c3fc3f7b24274df301dcaa18247d6bfab6e
5hpik <maxim.loboda2013@yandex.ru>
2023-12-17 17:25:52 +0700

    Почти доделал все задания

---
+++ task5/task5.py
@@ -0,0 +1,65 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QColorDialog
+from PyQt5.QtGui import QTransform
+import sys
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('task5.ui', self)
+        self.setFixedSize(350, 290)
+
+        self.checkbox_actions = {
+            'firstBox': self.change_color,
+            'secondBox': self.change_text,
+            'thirdBox': self.enlarge_button,
+            'fourthBox': self.make_button_invisible,
+            'fifthBox': self.set_inactive
+        }
+
+        for checkbox, action in self.checkbox_actions.items():
+            getattr(self, checkbox).stateChanged.connect(action)
+
+    def change_color(self):
+        if self.firstBox.isChecked():
+            color = QColorDialog.getColor()
+            self.pushButton.setStyleSheet(f'background-color: {color.name()}')
+        else:
+            self.pushButton.setStyleSheet('QPushButton {background-color: #fdfdfd}')
+
+    def change_text(self):
+        if self.secondBox.isChecked():
+            self.pushButton.setText('New Text')
+        else:
+            self.pushButton.setText('Accept')
+
+    def enlarge_button(self):
+        if self.thirdBox.isChecked():
+            self.pushButton.setGeometry(120, 200, 186, 56)
+        else:
+            self.pushButton.setGeometry(120, 200, 93, 28)
+
+    def make_button_invisible(self):
+        if self.fourthBox.isChecked():
+            self.pushButton.setVisible(False)
+        else:
+            self.pushButton.setVisible(True)
+
+    def set_inactive(self):
+        if self.fifthBox.isChecked():
+            self.pushButton.setEnabled(False)
+        else:
+            self.pushButton.setEnabled(True)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec_())
---
+++ task5/task5_test.py
@@ -0,0 +1,67 @@
+import pytest
+from PyQt5.QtCore import Qt
+from task5 import MyWidget
+
+
+@pytest.fixture
+def app(qtbot):
+    test_app = MyWidget()
+    qtbot.addWidget(test_app)
+    return test_app
+
+
+def test_initial_state(app):
+    assert not app.firstBox.isChecked()
+    assert not app.secondBox.isChecked()
+    assert not app.thirdBox.isChecked()
+    assert not app.fourthBox.isChecked()
+    assert not app.fifthBox.isChecked()
+    assert app.pushButton.isVisible()
+    assert app.pushButton.isEnabled()
+    assert app.pushButton.text() == "Accept"
+
+
+def test_show_button(app, qtbot):
+    app.fourthBox.setChecked(True)
+    qtbot.mouseClick(app.fourthBox, Qt.LeftButton)
+    
+    assert app.pushButton.isVisible()
+
+
+def test_disable_button(app, qtbot):
+    qtbot.mouseClick(app.fifthBox, Qt.LeftButton)
+    
+    assert not app.pushButton.isEnabled()
+
+
+def test_enlarge_button(app, qtbot):
+    initial_size = app.pushButton.size()
+    qtbot.mouseClick(app.thirdBox, Qt.LeftButton)
+    
+    assert app.pushButton.size() == initial_size * 2
+
+
+def test_change_button_color(app, qtbot):
+    qtbot.mouseClick(app.firstBox, Qt.LeftButton)
+    
+    assert 'background-color' in app.pushButton.styleSheet()
+
+
+def test_reset_button_color(app, qtbot):
+    app.firstBox.setChecked(True)
+    qtbot.mouseClick(app.firstBox, Qt.LeftButton)
+    
+    assert "background-color: #fdfdfd" in app.pushButton.styleSheet()
+
+
+def test_change_button_text(app, qtbot):
+    qtbot.mouseClick(app.secondBox, Qt.LeftButton)
+    
+    assert app.pushButton.text() == "New Text"
+
+
+def test_reset_button_text(app, qtbot):
+    app.secondBox.setChecked(True)
+    qtbot.mouseClick(app.secondBox, Qt.LeftButton)
+    
+    assert app.pushButton.text() == "Accept"
